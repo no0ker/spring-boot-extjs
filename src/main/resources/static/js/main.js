14694529ed7a3f77b95a6a9132fc61393e641e19
Рустам <rshavaliev@diasoft.ru>
2017-07-29 02:48:28 +0500

    commit message

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,162 +1,400 @@
-// --- Define Data Store
-
-Ext.define('Employees', {
-    extend: 'Ext.data.Store',
-    alias: 'store.employees',
-
-    proxy: {
-        type: 'ajax',
-        url: 'data1.json'
-    }
-});
-
-Ext.define('PopupForm', {
+Ext.define('Writer.Form', {
     extend: 'Ext.form.Panel',
-    xtype: 'popupform',
-    controller: 'popupform',
+    alias: 'widget.writerform',
 
-    title: 'Update Record',
+    requires: ['Ext.form.field.Text'],
 
-    width: 300,
-    floating: true,
-    centered: true,
-    modal: true,
-
-    items: [{
-        xtype: 'textfield',
-        name: 'firstname',
-        label: 'First Name',
-        bind: '{employee.firstName}'
-    }, {
-        xtype: 'textfield',
-        name: 'lastname',
-        label: 'Last Name',
-        bind: '{employee.lastName}'
-
-    }, {
-        xtype: 'textfield',
-        name: 'phonenumber',
-        label: 'Phone Number',
-        bind: '{employee.phoneNumber}'
-
-    }, {
-        xtype: 'selectfield',
-        name: 'office',
-        label: 'Office Location',
-        bind: '{employee.officeLocation}',
-        options: [{
-            text: "Redwood City, CA",
-            value: 'rwcca'
-        }, {
-            text: "Lawrence, KS",
-            value: 'lk'
-        }, {
-            text: "Frederick, MD",
-            value: 'fmd'
-        }],
-        defaultTabletPickerConfig: {
-            height: 200
-        }
-    }, {
-        xtype: 'toolbar',
-        docked: 'bottom',
-        items: ['->', {
-            xtype: 'button',
-            text: 'Submit',
-            iconCls: 'x-fa fa-check',
-            handler: 'submitUpdate'
-        }, {
-            xtype: 'button',
-            text: 'Cancel',
-            iconCls: 'x-fa fa-close',
-            handler: 'cancelUpdate'
-        }]
-    }]
-});
-
-
-Ext.define('PopupFormController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.popupform',
-
-    cancelUpdate: function () {
-        var view = this.getView(),
-            record = view.getRecord();
-
-        view.destroy();
-        record.reject();
-    },
-
-    submitUpdate: function(me) {
-        var view = this.getView(),
-            record = view.getRecord();
-
-        view.destroy();
-        record.commit();
-    }
-});
-
-//----------------------------------------------------
-
-Ext.define('MyListViewController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.listview',
-
-    onPopupForm: function (view, index, item, record) {
-        Ext.Viewport.add({
-            xtype: 'popupform',
-            width: 400,
-            record: record,
-            viewModel : {
-                data: {
-                    employee: record
-                }
-            }
-        });
-    }
-});
-
-Ext.application({
-    name: 'Sencha Employee Directory',
-
-    launch: function() {
-        Ext.Viewport.add({
-            xtype: 'tabpanel',
-            controller: 'listview',
-
+    initComponent: function(){
+        Ext.apply(this, {
+            activeRecord: null,
+            iconCls: 'icon-user',
+            frame: true,
+            title: 'User -- All fields are required',
+            defaultType: 'textfield',
+            bodyPadding: 5,
+            fieldDefaults: {
+                anchor: '100%',
+                labelAlign: 'right'
+            },
             items: [{
-                title: 'Employee Directory',
-                xtype: 'grid',
-                iconCls: 'x-fa fa-users',
-                grouped: true,
-                listeners: {
-                    itemtap: 'onPopupForm'
-                },
-                store: {
-                    type: 'employees',
-                    autoLoad: true,
-                    sorters: ['firstName', 'lastName', 'officeLocation'],
-                    grouper: 'officeLocation'
-                },
-                columns: [{
-                    text: 'First Name',
-                    dataIndex: 'firstName',
-                    flex: 1
+                fieldLabel: 'Email',
+                name: 'email',
+                allowBlank: false,
+                vtype: 'email'
+            }, {
+                fieldLabel: 'First',
+                name: 'first',
+                allowBlank: false
+            }, {
+                fieldLabel: 'Last',
+                name: 'last',
+                allowBlank: false
+            }],
+            dockedItems: [{
+                xtype: 'toolbar',
+                dock: 'bottom',
+                ui: 'footer',
+                items: ['->', {
+                    iconCls: 'icon-save',
+                    itemId: 'save',
+                    text: 'Save',
+                    disabled: true,
+                    scope: this,
+                    handler: this.onSave
                 }, {
-                    text: 'Last Name',
-                    dataIndex: 'lastName',
-                    flex: 1
+                    iconCls: 'icon-user-add',
+                    text: 'Create',
+                    scope: this,
+                    handler: this.onCreate
                 }, {
-                    text: 'Phone Number',
-                    dataIndex: 'phoneNumber',
-                    flex: 1
+                    iconCls: 'icon-reset',
+                    text: 'Reset',
+                    scope: this,
+                    handler: this.onReset
                 }]
-            },{
-                title: 'About Sencha',
-                padding: 20,
-                iconCls: 'x-fa fa-info-circle',
-                html: '<h1>About Sencha</h1><br/>More than 10,000 customers and 60% of the Fortune 100 rely on Sencha solutions to deliver innovative applications that drive their businesses. With a longstanding commitment to web technologies, Sencha dramatically reduces the cost and complexity of developing and delivering enterprise applications across multiple device types.<br/><br/><h2>Create feature-rich HTML5 applications using JavaScript</h2><br/>Sencha Ext JS is the most comprehensive MVC/MVVM JavaScript framework for building feature-rich, cross-platform web applications targeting desktops, tablets, and smartphones. Ext JS leverages HTML5 features on modern browsers while maintaining compatibility and functionality for legacy browsers.<br/><br/>Ext JS features hundreds of high-performance UI widgets that are meticulously designed to fit the needs of the simplest as well as the most complex web applications. Ext JS templates and layout manager give you full control over your display irrespective of devices and screen sizes. An advanced charting package allows you to visualize large quantities of data. The framework includes a robust data package that can consume data from any backend data source. Ext JS also offers several out-of-the-box themes, and complete theming support that lets you build applications that reflect your brand. It also includes an accessibility package (ARIA) to help with Section 508 compliance.'
             }]
         });
+        this.callParent();
+    },
+
+    setActiveRecord: function(record){
+        this.activeRecord = record;
+        if (record) {
+            this.down('#save').enable();
+            this.getForm().loadRecord(record);
+        } else {
+            this.down('#save').disable();
+            this.getForm().reset();
+        }
+    },
+
+    onSave: function(){
+        var active = this.activeRecord,
+            form = this.getForm();
+
+        if (!active) {
+            return;
+        }
+        if (form.isValid()) {
+            form.updateRecord(active);
+            this.onReset();
+        }
+    },
+
+    onCreate: function(){
+        var form = this.getForm();
+
+        if (form.isValid()) {
+            this.fireEvent('create', this, form.getValues());
+            form.reset();
+        }
+
+    },
+
+    onReset: function(){
+        this.setActiveRecord(null);
+        this.getForm().reset();
     }
+});
+
+Ext.define('Writer.Grid', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.writergrid',
+
+    requires: [
+        'Ext.grid.plugin.CellEditing',
+        'Ext.form.field.Text',
+        'Ext.toolbar.TextItem'
+    ],
+
+    initComponent: function(){
+
+        this.editing = Ext.create('Ext.grid.plugin.CellEditing');
+
+        Ext.apply(this, {
+            iconCls: 'icon-grid',
+            frame: true,
+            plugins: [this.editing],
+            dockedItems: [{
+                xtype: 'toolbar',
+                items: [{
+                    iconCls: 'icon-add',
+                    text: 'Add',
+                    scope: this,
+                    handler: this.onAddClick
+                }, {
+                    iconCls: 'icon-delete',
+                    text: 'Delete',
+                    disabled: true,
+                    itemId: 'delete',
+                    scope: this,
+                    handler: this.onDeleteClick
+                }]
+            }, {
+                weight: 2,
+                xtype: 'toolbar',
+                dock: 'bottom',
+                items: [{
+                    xtype: 'tbtext',
+                    text: '<b>@cfg</b>'
+                }, '|', {
+                    text: 'autoSync',
+                    enableToggle: true,
+                    pressed: true,
+                    tooltip: 'When enabled, Store will execute Ajax requests as soon as a Record becomes dirty.',
+                    scope: this,
+                    toggleHandler: function(btn, pressed){
+                        this.store.autoSync = pressed;
+                    }
+                }, {
+                    text: 'batch',
+                    enableToggle: true,
+                    pressed: true,
+                    tooltip: 'When enabled, Store will batch all records for each type of CRUD verb into a single Ajax request.',
+                    scope: this,
+                    toggleHandler: function(btn, pressed){
+                        this.store.getProxy().batchActions = pressed;
+                    }
+                }, {
+                    text: 'writeAllFields',
+                    enableToggle: true,
+                    pressed: false,
+                    tooltip: 'When enabled, Writer will write *all* fields to the server -- not just those that changed.',
+                    scope: this,
+                    toggleHandler: function(btn, pressed){
+                        this.store.getProxy().getWriter().writeAllFields = pressed;
+                    }
+                }]
+            }, {
+                weight: 1,
+                xtype: 'toolbar',
+                dock: 'bottom',
+                ui: 'footer',
+                items: ['->', {
+                    iconCls: 'icon-save',
+                    text: 'Sync',
+                    scope: this,
+                    handler: this.onSync
+                }]
+            }],
+            columns: [{
+                text: 'ID',
+                width: 40,
+                sortable: true,
+                resizable: false,
+                draggable: false,
+                hideable: false,
+                menuDisabled: true,
+                dataIndex: 'id',
+                renderer: function(value){
+                    return Ext.isNumber(value) ? value : '&nbsp;';
+                }
+            }, {
+                header: 'Email',
+                flex: 1,
+                sortable: true,
+                dataIndex: 'email',
+                field: {
+                    type: 'textfield'
+                }
+            }, {
+                header: 'First',
+                width: 100,
+                sortable: true,
+                dataIndex: 'first',
+                field: {
+                    type: 'textfield'
+                }
+            }, {
+                header: 'Last',
+                width: 100,
+                sortable: true,
+                dataIndex: 'last',
+                field: {
+                    type: 'textfield'
+                }
+            }]
+        });
+        this.callParent();
+        this.getSelectionModel().on('selectionchange', this.onSelectChange, this);
+    },
+
+    onSelectChange: function(selModel, selections){
+        this.down('#delete').setDisabled(selections.length === 0);
+    },
+
+    onSync: function(){
+        this.store.sync();
+    },
+
+    onDeleteClick: function(){
+        var selection = this.getView().getSelectionModel().getSelection()[0];
+        if (selection) {
+            this.store.remove(selection);
+        }
+    },
+
+    onAddClick: function(){
+        var rec = new Writer.Person({
+            first: '',
+            last: '',
+            email: ''
+        }), edit = this.editing;
+
+        edit.cancelEdit();
+        this.store.insert(0, rec);
+        edit.startEditByPosition({
+            row: rec,
+            column: 1
+        });
+    }
+});
+
+Ext.define('Writer.Person', {
+    extend: 'Ext.data.Model',
+    fields: [{
+        name: 'id',
+        type: 'int',
+        useNull: true
+    }, 'email', 'first', 'last'],
+    validators: {
+        email: {
+            type: 'length',
+            min: 1
+        },
+        first: {
+            type: 'length',
+            min: 1
+        },
+        last: {
+            type: 'length',
+            min: 1
+        }
+    }
+});
+
+Ext.require([
+    'Ext.data.*',
+    'Ext.tip.QuickTipManager',
+    'Ext.window.MessageBox'
+]);
+
+Ext.onReady(function(){
+    Ext.tip.QuickTipManager.init();
+
+    Ext.create('Ext.button.Button', {
+        margin: '0 0 20 20',
+        text: 'Reset sample database back to initial state',
+        renderTo: document.body,
+        tooltip: 'The sample database is stored in the session, including any changes you make. Click this button to reset the sample database to the initial state',
+        handler: function(){
+            Ext.getBody().mask('Resetting...');
+            Ext.Ajax.request({
+                url: 'rest/notes',
+                callback: function(options, success, response) {
+                    Ext.getBody().unmask();
+
+                    var didReset = true,
+                        o;
+
+                    if (success) {
+                        try {
+                            o = Ext.decode(response.responseText);
+                            didReset = o.success === true;
+                        } catch (e) {
+                            didReset = false;
+                        }
+                    } else {
+                        didReset = false;
+                    }
+
+                    if (didReset) {
+                        store.load();
+                        main.down('#form').setActiveRecord(null);
+                        Ext.example.msg('Reset', 'Reset successful');
+                    } else {
+                        Ext.MessageBox.alert('Error', 'Unable to reset example database');
+                    }
+
+                }
+            });
+        }
+    });
+
+    var store = Ext.create('Ext.data.Store', {
+        model: 'Writer.Person',
+        autoLoad: true,
+        autoSync: true,
+        proxy: {
+            type: 'ajax',
+            api: {
+                read: 'rest/notes',
+                create: 'app.php/users/create',
+                update: 'app.php/users/update',
+                destroy: 'app.php/users/destroy'
+            },
+            reader: {
+                type: 'json',
+                successProperty: 'success',
+                root: 'data',
+                messageProperty: 'message'
+            },
+            writer: {
+                type: 'json',
+                writeAllFields: false,
+                root: 'data'
+            },
+            listeners: {
+                exception: function(proxy, response, operation){
+                    Ext.MessageBox.show({
+                        title: 'REMOTE EXCEPTION',
+                        msg: operation.getError(),
+                        icon: Ext.MessageBox.ERROR,
+                        buttons: Ext.Msg.OK
+                    });
+                }
+            }
+        },
+        listeners: {
+            write: function(proxy, operation){
+                if (operation.action == 'destroy') {
+                    main.child('#form').setActiveRecord(null);
+                }
+                Ext.example.msg(operation.action, operation.getResultSet().message);
+            }
+        }
+    });
+
+    var main = Ext.create('Ext.container.Container', {
+        padding: '0 0 0 20',
+        width: 500,
+        height: Ext.themeName === 'neptune' ? 700 : 650,
+        renderTo: document.body,
+        layout: {
+            type: 'vbox',
+            align: 'stretch'
+        },
+        items: [{
+            itemId: 'form',
+            xtype: 'writerform',
+            manageHeight: false,
+            margin: '0 0 10 0',
+            listeners: {
+                create: function(form, data){
+                    store.insert(0, data);
+                }
+            }
+        }, {
+            itemId: 'grid',
+            xtype: 'writergrid',
+            title: 'User List',
+            flex: 1,
+            store: store,
+            listeners: {
+                selectionchange: function(selModel, selected) {
+                    main.child('#form').setActiveRecord(selected[0] || null);
+                }
+            }
+        }]
+    });
 });
